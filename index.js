8b3135e66f7f907283d089cbb8286945ec9b2742
JHanslik <julien.hanslik@gmail.com>
2022-07-07 09:57:37 +0200

    first commit after creation of new repository due to bug

---
+++ index.js
@@ -0,0 +1,53 @@
+// VARIABLES
+const todoImput = document.querySelector(".todo-input")
+const todoButton = document.querySelector(".todo-button")
+const todoList = document.querySelector(".todo-list")
+
+// ÉCOUTEURS
+// todoButton.addEventListener("click", onTaskSubmit)
+
+// FUNCTIONS
+const onTaskSubmit = (event) => {
+    event.preventDefault()
+
+    // Todo DIV
+    const todoDiv = document.createElement("div")
+    todoDiv.classList.add ("todo")
+
+        // Créer le Select
+        const newSelect = document.createElement("div")
+        newSelect.innerHTML = `<select name="" id="">
+        <option value="todo">To do</option>
+        <option value="doing">Doing</option>
+        <option value="done">Done (fait)</option>
+    </select>`
+        newSelect.classList.add("status")
+        todoDiv.appendChild(newSelect)
+
+        // Li creation
+        const newTodo = document.createElement("li")
+        newTodo.innerText = "Hey"
+        newTodo.classList.add("todo-item")
+        todoDiv.appendChild(newTodo)
+
+        // Priority number
+        const newPriority = document.createElement("li")
+        newPriority.innerText = `5`
+        newPriority.classList.add("item-priority")
+        todoDiv.appendChild(newPriority)
+
+        // modification button
+        const modificationButton = document.createElement("button")
+        modificationButton.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>'
+        modificationButton.classList.add("modification-button")
+        todoDiv.appendChild(modificationButton)
+
+        // Delete Button
+        const eraseButton = document.createElement("button")
+        eraseButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+        eraseButton.classList.add("erase-button")
+        todoDiv.appendChild(eraseButton)
+
+    // ToDo add to the list
+    todoList.appendChild(todoDiv)
+}
