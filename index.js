1bd8c9391f9b39457ea8521dd510f6a7d00fc95e
JHanslik <julien.hanslik@gmail.com>
2022-07-07 17:35:22 +0200

    overhall JS file

--- index.js
+++ index.js
@@ -6,71 +6,74 @@ const todoPriority = document.querySelector("#priority");
 const filterOption = document.querySelector("filter");
 // const todoStatus = document.querySelector(".status")
 
+let tasksArray = []
+
+
 // FUNCTIONS
 const onTaskSubmit = (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Todo DIV
-  const todoDiv = document.createElement("li");
-  todoDiv.classList.add("todo");
+    // ajout valeur au tableau
+    tasksArray.push ({task: todoImput.value, priority: todoPriority.value, status:"To do"})
+    console.log (tasksArray)
+    todoList.innerHTML=""
+    tasksArray.forEach ((task, i) => {
+        todoDiv = document.createElement("li")
+        todoList.appendChild(todoDiv)
+        // Todo Li
+        // const todoDiv = document.createElement("li");
+        todoDiv.classList.add("todo");
+        todoDiv.innerHTML = `<div class="status">
+        <select name="" id="select-${i}">
+        <option value="todo">To do</option>
+        <option value="doing">Doing</option>
+        <option value="done">Done</option>
+        </select>
+        </div>
+        
+        <p class = "todo-item">${tasksArray[i].task}</p>
+        
+        <p class="item-priority">${tasksArray[i].priority}</p>
+        
+        <button class="modification-button">
+        <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+        
+        <button class="erase-button" id="erase-${i}">
+        <i class="fa-solid fa-trash-can"></i>
+        </button>
+        `
+        const newSelect = document.getElementById(`select-${i}`)
+        newSelect.addEventListener("change", (e) => {
+            completion (e, i)
+        })
+        console.log (tasksArray[i].task)
 
-  // Créer le Select
-  const newSelect = document.createElement("div");
-  newSelect.innerHTML = `<select name="" id="">
-  <option value="todo">To do</option>
-  <option value="doing">Doing</option>
-  <option value="done">Done</option>
-</select>`;
-  newSelect.classList.add("status");
-  newSelect.addEventListener("change", completion);
-  todoDiv.appendChild(newSelect);
-
-  // Li creation
-  const newTodo = document.createElement("p");
-  newTodo.innerText = todoImput.value;
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo);
-
-  // Priority number
-  const newPriority = document.createElement("p");
-  newPriority.innerText = todoPriority.value;
-  newPriority.classList.add("item-priority");
-  todoDiv.appendChild(newPriority);
-
-  // modification button
-  const modificationButton = document.createElement("button");
-  modificationButton.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
-  modificationButton.classList.add("modification-button");
-  todoDiv.appendChild(modificationButton);
-
-  // Delete Button
-  const eraseButton = document.createElement("button");
-  eraseButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-  eraseButton.classList.add("erase-button");
-  todoDiv.appendChild(eraseButton);
-
-  // ToDo add to the list
-  todoList.appendChild(todoDiv);
-
-  //  Reset de la value
-  todoImput.value = "";
-  todoPriority.value = "";
-};
-
+        const delLine = document.getElementById(`erase-${i}`)
+        delLine.addEventListener("click", () => {
+            deleteChecked(i)
+        })
+        console.log()
+        
+    // Reset de la value
+    
+})
+todoImput.value = "";
+todoPriority.value = ""
+    // console.log (todoDiv)
+}
+    
 //  Button Part
 
-const deleteChecked = (e) => {
+const deleteChecked = (e, i) => {
   const item = e.target;
 //   console.log(item);
   //   Delete Part
   if (item.classList[0] === "erase-button") {
     // supprimer toute la barre
     const todo = item.parentElement;
-    // todo.classList.add("fall")
-    // todo.addEventListener("transitionend", function() {
-    //     todo.remove ()
-    // })
     todo.remove();
+    tasksArray[i].remove()
   }
   //   modification part
   if (item.classList[0] === "modification-button") {
@@ -79,14 +82,14 @@ const deleteChecked = (e) => {
 
   }
 };
-todoList.addEventListener("click", deleteChecked);
 
-const completion = (e) => {
-  console.log(e.target.value);
+const completion = (e, i) => {
+  console.log(i);
   const todo = e.target.parentElement.parentElement;
   if (e.target.value === "done") {
     todo.classList.add("completed");
   } else {
     todo.classList.remove("completed");
   }
-};
+
+}
