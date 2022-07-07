1e169689905cdb9c6a652bbbeca473e6e6e9ee23
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-07-07 10:19:56 +0200

    Add task done

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
 // VARIABLES
-const todoImput = document.querySelector(".todo-input")
-const todoButton = document.querySelector(".todo-button")
-const todoList = document.querySelector(".todo-list")
+const todoImput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 // ÉCOUTEURS
 // todoButton.addEventListener("click", onTaskSubmit)
 
 // FUNCTIONS
 const onTaskSubmit = (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    // Todo DIV
-    const todoDiv = document.createElement("div")
-    todoDiv.classList.add ("todo")
+  // Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
 
-        // Créer le Select
-        const newSelect = document.createElement("div")
-        newSelect.innerHTML = `<select name="" id="">
+  // Créer le Select
+  const newSelect = document.createElement("div");
+  newSelect.innerHTML = `<select name="" id="">
         <option value="todo">To do</option>
         <option value="doing">Doing</option>
         <option value="done">Done (fait)</option>
-    </select>`
-        newSelect.classList.add("status")
-        todoDiv.appendChild(newSelect)
+    </select>`;
+  newSelect.classList.add("status");
+  todoDiv.appendChild(newSelect);
 
-        // Li creation
-        const newTodo = document.createElement("li")
-        newTodo.innerText = "Hey"
-        newTodo.classList.add("todo-item")
-        todoDiv.appendChild(newTodo)
+  // Li creation
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoImput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
 
-        // Priority number
-        const newPriority = document.createElement("li")
-        newPriority.innerText = `5`
-        newPriority.classList.add("item-priority")
-        todoDiv.appendChild(newPriority)
+  // Priority number
+  const newPriority = document.createElement("li");
+  newPriority.innerText = `5`;
+  newPriority.classList.add("item-priority");
+  todoDiv.appendChild(newPriority);
 
-        // modification button
-        const modificationButton = document.createElement("button")
-        modificationButton.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>'
-        modificationButton.classList.add("modification-button")
-        todoDiv.appendChild(modificationButton)
+  // modification button
+  const modificationButton = document.createElement("button");
+  modificationButton.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+  modificationButton.classList.add("modification-button");
+  todoDiv.appendChild(modificationButton);
 
-        // Delete Button
-        const eraseButton = document.createElement("button")
-        eraseButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
-        eraseButton.classList.add("erase-button")
-        todoDiv.appendChild(eraseButton)
+  // Delete Button
+  const eraseButton = document.createElement("button");
+  eraseButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  eraseButton.classList.add("erase-button");
+  todoDiv.appendChild(eraseButton);
 
-    // ToDo add to the list
-    todoList.appendChild(todoDiv)
-}
+  // ToDo add to the list
+  todoList.appendChild(todoDiv);
+
+  //  Reset de la value
+  todoImput.value = "";
+};
